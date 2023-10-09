9c08cf3a3ae343a3c0306f9ac2c7e6c6f806e91c
teddy-webdev <tshikombadaniel@icloud.com>
2023-10-09 14:25:39 +0300

    translation

--- assets/scripts/translation.js
+++ assets/scripts/translation.js
@@ -1,5 +1,4 @@
 let lang = document.getElementById("lang");
-// let langCurrentImg = document.getElementById("currentLang");
 
 function toggleLangBtn() {
   if (lang.className === "langList") {
@@ -12,16 +11,16 @@ function toggleLangBtn() {
   }
 }
 
-$.i18n = function (options) {
-  options = $.extend(
+function i18n(options) {
+  options = Object.assign(
     {},
     {
       lang: window.localStorage.getItem("language") || "en",
-      data: $.i18n,
+      data: i18nData,
     },
     options
   );
-  var langStore = options.data[options.lang];
+  let langStore = options.data[options.lang];
   this.setLang = function (language) {
     window.localStorage.setItem("language", language);
     langStore = options.data[language];
@@ -30,108 +29,85 @@ $.i18n = function (options) {
     return langStore[key];
   };
   return this;
+}
+
+const i18nData = {
+  en: {
+    lang_icon: "<img src='https://flagcdn.com/48x36/gb.png' style='width: 1.25rem; height: 1rem;' alt='lang'>",
+    home_nav: "Home",
+    menu_nav: "Menu",
+    about_us: "About",
+    contact: "Contact",
+    reservation: "Reservation",
+  },
+  tr: {
+    lang_icon: "<img src='https://flagcdn.com/48x36/tr.png' style='width: 1.25rem; height: 1rem;' alt='lang'>",
+    home_nav: "Ana sayfa",
+    menu_nav: "Menu",
+    about_us: "About",
+    contact: "Contact",
+    reservation: "Rezervation",
+  },
 };
 
-$.i18n.tr = {
-  lang_icon:
-    "<img src='https://flagcdn.com/48x36/tr.png' style='width: 1.25rem; height: 1rem;' alt='lang'>",
-  home_nav: "Ana sayfa",
-  menu_nav: "Menu",
-  about_us: "About",
-  contact: "Contact",
-  reservation: "Rezervation",
-};
-$.i18n.ru = {
-  lang_icon:
-    "<img src='https://flagcdn.com/48x36/ru.png' style='width: 1.25rem; height: 1rem;' alt='lang'>",
-  home_nav: "Ana sayfa",
-  menu_nav: "Menu",
-  about_us: "About",
-  contact: "Contact",
-  reservation: "Rezervation",
-};
-$.i18n.en = {
-  lang_icon:
-    "<img src='https://flagcdn.com/48x36/gb.png' style='width: 1.25rem; height: 1rem;' alt='lang'>",
-  home_nav: "Ana sayfa",
-  menu_nav: "Menu",
-  about_us: "About",
-  contact: "Contact",
-  reservation: "Rezervation",
-};
+const i18nInstance = new i18n();
 
-var i18n = $.i18n();
-
-var changeLabels = function () {
-  $("[data-translate]").each(function () {
-    var forLabel = $(this).attr("data-translate");
-    this.innerHTML = i18n.getItem(forLabel);
+function changeLabels() {
+  const elements = document.querySelectorAll("[data-translate]");
+  elements.forEach(function (element) {
+    const forLabel = element.getAttribute("data-translate");
+    element.innerHTML = i18nInstance.getItem(forLabel);
   });
-};
+}
 
 function getParameterByName(name, url = window.location.href) {
   name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  const results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return "";
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 function setContactFormPlaceHolders(lang) {
+  // Replace this code with your desired translations
+  // Example:
   // if (lang === "tr") {
-  //     $('input[name="fullName"]').attr("placeholder", "Enter your Name");
-  //     $('input[name="email"]').attr("placeholder", "Enter your Email");
-  //     $('input[name="EMAIL"]').attr("placeholder", "Your Email");
-  //     $('textarea[name="message"]').attr("placeholder", "Enter your Message");
-  //   }
-  //   if (lang === "en") {
-  //     $('input[name="fullName"]').attr("placeholder", "Enter your Name");
-  //     $('input[name="email"]').attr("placeholder", "Enter your Email");
-  //     $('input[name="EMAIL"]').attr("placeholder", "Your Email");
-  //     $('textarea[name="message"]').attr("placeholder", "Enter your Message");
-  //   }
-  //   if (lang === "ru") {
-  //     $('input[name="fullName"]').attr("placeholder", "Enter your Name");
-  //     $('input[name="email"]').attr("placeholder", "Enter your Email");
-  //     $('input[name="EMAIL"]').attr("placeholder", "Your Email");
-  //     $('textarea[name="message"]').attr("placeholder", "Enter your Message");
-  //   }
+  //   document.querySelector('input[name="fullName"]').placeholder = "Enter your Name";
+  //   document.querySelector('input[name="email"]').placeholder = "Enter your Email";
+  //   // ...
+  // }
 }
 
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
   const lang = getParameterByName("lang");
   if (lang) {
     window.localStorage.setItem("language", lang);
-    i18n.setLang(lang);
+    i18nInstance.setLang(lang);
   }
   changeLabels();
-  setContactFormPlaceHolders(window.localStorage.getItem("language") || "en");
+  // setContactFormPlaceHolders(window.localStorage.getItem("language") || "en");
 
-  // $("dropdownMenuButton").text(
-  //     (window.localStorage.getItem("language") || "en").toUpperCase()
-  // );
+  // document.querySelector('.dropdownMenuButton').textContent = (window.localStorage.getItem("language") || "en").toUpperCase();
 });
 
-$("#lang")
-  .find("a")
-  .click(function (e) {
-    var lang = this.href.slice(-2);
-    i18n.setLang(lang);
-    setContactFormPlaceHolders(lang);
-    // $(".dropdownMenuButton").text(
-    //     (window.localStorage.getItem("language") || "en").toUpperCase()
-    // );
+document.getElementById("lang").addEventListener("click", function (e) {
+  if (e.target.tagName === "A") {
+    const lang = e.target.href.slice(-2);
+    i18nInstance.setLang(lang);
+    // setContactFormPlaceHolders(lang);
+    // document.querySelector('.dropdownMenuButton').textContent = (window.localStorage.getItem("language") || "en").toUpperCase();
 
     const langQueryParam = getParameterByName("lang");
     if (langQueryParam) {
-      var url = new URL(window.location.href);
-      var search_params = url.searchParams;
-      search_params.set("lang", lang);
-      url.search = search_params.toString();
-      var new_url = url.pathname + url.search;
-      window.history.replaceState({}, "", new_url);
+      const url = new URL(window.location.href);
+      const searchParams = url.searchParams;
+      searchParams.set("lang", lang);
+      url.search = searchParams.toString();
+      const newUrl = url.pathname + url.search;
+      window.history.replaceState({}, "", newUrl);
     }
     changeLabels();
     e.preventDefault();
-  });
+  }
+});
