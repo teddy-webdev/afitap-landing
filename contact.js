7ae97000b586b2828c74aa9d336405dc73f02565
teddy-webdev <tshikombadaniel@icloud.com>
2023-10-10 12:51:37 +0300

    translation for all pages done, expect menu items details

--- contact.js
+++ contact.js
@@ -1,56 +1,42 @@
-  
-// script.js
-const msgDone = document.querySelector(".w-form-done");
-const msgFail = document.querySelector(".w-form-fail");
+$(document).ready(function() {
+  const msgDone = $(".w-form-done");
+  const msgFail = $(".w-form-fail");
 
-const sendButton = document.querySelector("#contact_button");
-sendButton.addEventListener("click", function (e) {
-  e.preventDefault();
-  var email = document.forms["wf-form-Styleguide-Form"]["email"].value;
-  var formContact = document.getElementById("wf-form-Styleguide-Form");
-  var url = "https://script.google.com/macros/s/AKfycbxNS5CZ-Jf7Kh9u3gm2Rv2rI5U1dqYRZakbWGJtYPe6k-1J-F2shnzdFsNBd_RRh2t1/exec";
+  $("#contact_button").click(function(e) {
+    e.preventDefault();
+    var email = $("#email").val();
+    var formContact = $("#wf-form-Styleguide-Form");
+    var url = "https://script.google.com/macros/s/AKfycbz6j4Is9weaBxC40-swpZLcoD-y96e71GN5tgcdLsOkMc89bCeU9yXPIIaIKTTTp5v8/exec";
 
-  if (email === "") {
-    alert("Email must be filled out");
-    return;
-  }
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url + "?" + serializeForm(formContact), true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      console.log("data :", JSON.parse(xhr.responseText));
-      console.log("success");
-      msgDone.classList.remove("hide");
-
-      setTimeout(function () {
-        msgDone.classList.add("hide");
-        location.reload();
-      }, 3500);
-    } else {
-      console.log("error");
-      msgFail.classList.remove("hide");
-
-      setTimeout(function () {
-        msgFail.classList.add("hide");
-        location.reload();
-      }, 3500);
+    if (email === "") {
+      alert("Email must be filled out");
+      return;
     }
-  };
-  
-  xhr.send();
+
+    $.ajax({
+      url: url,
+      type: "GET",
+      data: formContact.serialize(),
+      contentType: "application/json",
+      success: function(data) {
+        console.log("data :", data);
+        console.log("success");
+        msgDone.removeClass("hide");
+
+        setTimeout(function() {
+          msgDone.addClass("hide");
+          location.reload();
+        }, 3500);
+      },
+      error: function() {
+        console.log("error");
+        msgFail.removeClass("hide");
+
+        setTimeout(function() {
+          msgFail.addClass("hide");
+          location.reload();
+        }, 3500);
+      }
+    });
+  });
 });
-
-function serializeForm(form) {
-  var formData = new FormData(form);
-  var params = [];
-
-  for (var pair of formData.entries()) {
-    params.push(encodeURIComponent(pair[0]) + "=" + encodeURIComponent(pair[1]));
-  }
-
-  return params.join("&");
-}
-
